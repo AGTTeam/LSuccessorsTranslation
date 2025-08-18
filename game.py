cf29f3b2647b1b2c9e7bf262c0260f5f4be0dcc6
Phantom <54214467+PhantomandGhost@users.noreply.github.com>
2025-08-18 11:45:25 +0100

    Add ï to replace character list

--- game.py
+++ game.py
@@ -16,6 +16,7 @@ def writeEncodedString(f, s, maxlen, encoding):
     s = s.replace("’", "]")
     s = s.replace("“", "{")
     s = s.replace("”", "}")
+    s = s.replace("ï", "$")
     # for name strings, there's actually a lot more space
     if f.tell() >= 0xa0730 and f.tell() < 0xa0a20:
         maxlen = 0xf
